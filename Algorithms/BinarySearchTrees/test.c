2bfd0831cf121356b86624c3efc2ddf9742390cb
alecksandr <sansepiol26@gmail.com>
2022-05-26 20:16:45 -0500

    Need to practice avl trees

--- Algorithms/BinarySearchTrees/test.c
+++ Algorithms/BinarySearchTrees/test.c
@@ -1,9 +1,31 @@
-
+#include <stdio.h>
 #include "./bistree.h"
+
+
+int compare (int *key1, int *key2)
+{
+    return *key1 == *key2;
+}
+
+
+void destroy (int *num)
+{
+    /* destroy the data */
+}
+
 
 /* testing the avl tree */
 int main ()
 {
+    BisTree mytree;
+    int n;
+    
+    
+    bistree_init(&mytree, &compare, &destroy);
+
+    n = 1;
+    bistree_insert(&mytree, &n);
+    
     
     return 0;
 }
