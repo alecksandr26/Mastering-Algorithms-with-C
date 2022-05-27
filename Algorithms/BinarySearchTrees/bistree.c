81d9b753690d98d433cd3d8b8a429aea1a62cff0
alecksandr <sansepiol26@gmail.com>
2022-05-26 20:09:57 -0500

    Lets test the avl tree

--- Algorithms/BinarySearchTrees/bistree.c
+++ Algorithms/BinarySearchTrees/bistree.c
@@ -1,5 +1,6 @@
 #include "./bistree.h"
 
+/* here define a function that will be declared */
 static void destroy_right (BisTree *tree, BiTreeNode *node);
 
 /* rotate left */
@@ -23,7 +24,7 @@ static void rotate_left (BiTreeNode **node)
         /* perfon an LR rotation */
         bitree_right(left) = bitree_left(grandchild);
         bitree_left(grandchild) = left;
-        bitree_left(*node) = bitree_right(gra ndchild);
+        bitree_left(*node) = bitree_right(grandchild);
         bitree_right(grandchild) = *node;
 
         /* depending on the factor we do */
@@ -53,12 +54,12 @@ static void rotate_left (BiTreeNode **node)
 /* rotate right */
 static void rotate_right (BiTreeNode **node)
 {
-    BiTreeNode = *right, *grandchild;
+    BiTreeNode *right, *grandchild;
 
     /* Gets the right child */
     right = bitree_right(*node);
 
-    if (((AlvNode *) bitree_data(right))->factor == AVL_RGT_HEAVY) {
+    if (((AvlNode *) bitree_data(right))->factor == AVL_RGT_HEAVY) {
         /* perform an RR rotation */
         bitree_left(right) = *node;
         ((AvlNode *) bitree_data(*node))->factor = AVL_BALANCED;
@@ -180,7 +181,7 @@ static int insert (BisTree *tree, BiTreeNode **node, const void *data, int *bala
         if (cmpval < 0) {
             /* move to the left */
             if (bitree_is_eob(bitree_left(*node))) {
-                if ((avl_data = (AvLNode *)malloc(sizeof(AvlNode))) == NULL)
+                if ((avl_data = (AvlNode *) malloc(sizeof(AvlNode))) == NULL)
                     return -1;
                 
                 avl_data->factor = AVL_BALANCED;
@@ -261,8 +262,6 @@ static int insert (BisTree *tree, BiTreeNode **node, const void *data, int *bala
 }
 
 
-
-
 /* hide: to mark node as hidden nodes */
 static int hide (BisTree *tree, BiTreeNode *node, const void *data)
 {
@@ -271,7 +270,7 @@ static int hide (BisTree *tree, BiTreeNode *node, const void *data)
     if (bitree_is_eob(node))
         return -1;
     
-    cmpval = tree->compare(data, ((AvlNode *)bitree_data(node))->datae);
+    cmpval = tree->compare(data, ((AvlNode *)bitree_data(node))->data);
 
     if (cmpval < 0)         /* move to the left */
         retval = hide(tree, bitree_left(node), data);
@@ -285,18 +284,21 @@ static int hide (BisTree *tree, BiTreeNode *node, const void *data)
     return retval;
 }
 
-
-static int lookup (BisTree *tree, BiTreeNode *node, void **data)
+/* lookup: A recursive function to find the data  */
+static int lookup (const BisTree *tree, BiTreeNode *node, void **data)
 {
     int cmpval, retval;
 
     if (bitree_is_eob(node))
         return -1;
 
+    /* use the user function to compare the data  */
     cmpval = tree->compare(*data, ((AvlNode *)bitree_data(node))->data);
-    if (cmpval < 0)
-        retval = lookup(tree, bistree_right(node), data);
+    
+    if (cmpval < 0) /* recursive case */
+        retval = lookup(tree, bitree_right(node), data);
     else {
+        
         if (!((AvlNode *)bitree_data(node))->hidden) {
             /* Pass back the data formthe tree */
             *data = ((AvlNode *)bitree_data(node))->data;
@@ -310,23 +312,35 @@ static int lookup (BisTree *tree, BiTreeNode *node, void **data)
 
 
 
-/* bistree_init: To Initializes the tree */
-void bistree_init (BisTree *tree, int (*compare)(const void *key1, const void *key2),
-                   void (*destroy)(void *data))
+/* bistree_init: Initializes the binary search tree specified by tree
+   tree: is the data structure
+   compare: specifies a user-defined function to compare elements
+   destroy: The destroy argument provides a way to free dynamically 
+   allocated data when bistree_destroy is called.
+   complexity: Is the O(1)
+*/
+void bistree_init (BisTree *tree, void (*compare)(const void *key1, const void *key2), void (*destroy)(void *data))
 {
     bitree_init(tree, destroy);
     tree->compare = compare;
 }
 
 
-/* bistree_destroy: To destory the binary tree */
+/* bistree_destroy: To destroy the structure easy 
+   tree: the data structure 
+   complexity: Is O(n) where n is the number of nodes that the tree has 
+ */
 void bistree_destroy (BisTree *tree)
 {
     destroy_left(tree, NULL);
     memset(tree, 0, sizeof(BisTree));
 }
 
-/* bistree_insert: To insert a node into the binary tree */
+/* bistree_insert: To insert data insde of the tree 
+   tree: is the data structure
+   data: The data stored in the binary tree
+   complexity: Is O(lg n) wheren is the nombre of nodes in the bianry search 
+*/
 int bistree_insert (BisTree *tree, const void *data)
 {
     int balanced = 0;
@@ -334,15 +348,25 @@ int bistree_insert (BisTree *tree, const void *data)
 }
 
 
-/* bistree_remove: To destroy the tree */
+/* bistree_remove: To remove one element 
+   tree: Is the data structure
+   data: The data to stored 
+   complexity: Is O(lg n) where is the number of node in the binary search
+*/
 int bistree_remove (BisTree *tree, const void *data)
 {
     return hide(tree, bitree_root(tree), data);
 }
 
 
-/* bistree_lookup: To if there is this data */
-int bistree_lookup (BisTree *tree, void **data)
+/* bistree_lookup: Determines whether a node matches data in the binary search
+   tree specified as tree. If a match is found, data points to the matching data in the
+   binary search tree upon return.
+   tree: Is the data structure 
+   data: Is the data found it 
+   complexity: Is O(lg n) where n is the number of nodes 
+ */
+int bistree_lookup (const BisTree *tree, void **data)
 {
-    return lookup(tree, bistree_root(tree), data);
+    return lookup(tree, bitree_root(tree), data);
 }
---
+++ Algorithms/BinarySearchTrees/test.c
@@ -0,0 +1,11 @@
+
+#include "./bistree.h"
+
+/* testing the avl tree */
+int main ()
+{
+    
+    return 0;
+}
+
+
