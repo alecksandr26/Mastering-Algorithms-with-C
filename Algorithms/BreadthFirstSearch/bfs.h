c89b7b6e1dc1ab87531f1d74f0dc2b59565bd947
alecksandr26 <sansepiol26@gmail.com>
2022-06-20 09:42:54 -0500

    Working with a bfs search

---
+++ Algorithms/BreadthFirstSearch/bfs.h
@@ -0,0 +1,21 @@
+
+#ifndef __BFS_H__
+#define __BFS_H__
+
+#include "../../DataStructures/Graphs/graphs.h"
+#include "../../DataStructures/LinkedList/linkedlist.h"
+
+
+typedef struct {
+    void *data;
+    VertexColor color;
+    int hops;
+} BfsVertex;
+
+
+
+
+
+
+
+#endif
